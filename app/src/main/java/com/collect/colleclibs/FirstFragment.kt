00d72fdca87f341e179a8b8e1f02970d60ffdfeb
tzj <249775302@qq.com>
2025-03-24 00:46:53 +0800

    v1.2.1

--- app/src/main/java/com/collect/colleclibs/FirstFragment.kt
+++ app/src/main/java/com/collect/colleclibs/FirstFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.collect.colleclibs.databinding.FragmentFirstBinding
+import com.permissionx.guolindev.PermissionX
 import com.sc.collectlibs.AppInfoUtil
 import com.sc.collectlibs.AppInfoUtil.GetDeviceInfo
 //import com.tbruyelle.rxpermissions3.RxPermissions
@@ -35,18 +37,23 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        binding.buttonFirst.setOnClickListener {
-//            RxPermissions(this)
-//                .requestEach(
-//                    Manifest.permission.READ_SMS,
-//                    Manifest.permission.READ_CONTACTS,
-//                    Manifest.permission.READ_CALL_LOG,
-//                    Manifest.permission.READ_PHONE_NUMBERS,
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                ).subscribe {
-//
-//                }
-//        }
+        binding.buttonFirst.setOnClickListener {
+            PermissionX.init(this)
+                .permissions(
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_CALL_LOG,
+                    Manifest.permission.READ_PHONE_NUMBERS,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                )
+                .request { allGranted, grantedList, deniedList ->
+                    if (allGranted) {
+                        Toast.makeText(requireActivity(), "All permissions are granted", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(requireActivity(), "These permissions are denied: $deniedList", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
         binding.buttonAppList.setOnClickListener {
             val data = AppInfoUtil.getAppList(requireContext())
             binding.textviewFirst.text = data
--- app/src/main/java/com/collect/colleclibs/MainActivity.kt
+++ app/src/main/java/com/collect/colleclibs/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.collect.colleclibs.databinding.ActivityMainBinding
-//import com.tbruyelle.rxpermissions3.RxPermissions
 
 class MainActivity : AppCompatActivity() {
 
